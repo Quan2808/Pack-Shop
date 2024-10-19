57debce7ee52f314599370c479ae2e0b5b1566d2
Nguyễn Trung Quân <nhotecfa@gmail.com>
2024-10-20 00:40:45 +0700

    refactor product list page

--- Client/src/main/java/com/packshop/client/controllers/dashboard/catalog/base/CatalogManageBaseController.java
+++ Client/src/main/java/com/packshop/client/controllers/dashboard/catalog/base/CatalogManageBaseController.java
@@ -2,7 +2,7 @@ package com.packshop.client.controllers.dashboard.catalog.base;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/dashboard/catalog") // định nghĩa rõ là path
+@RequestMapping("/dashboard/catalog")
 public interface CatalogManageBaseController {
     String CATALOG_PATH = "dashboard/pages/catalog";
 }

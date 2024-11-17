69b55aa54d7b7b4e5e21cefb4add70fb6eb79935
Nguyễn Trung Quân <116139754+Quan2808@users.noreply.github.com>
2024-11-17 22:59:38 +0700

    Delete API directory

--- API/src/main/java/com/packshop/api/entities/catalog/product/ProductStatus.java
+++
@@ -1,6 +0,0 @@
-package com.packshop.api.entities.catalog.product;
-
-public enum ProductStatus {
-    PUBLISHED,
-    INACTIVE
-}
--- API/src/main/java/com/packshop/api/repositories/catalog/product/ProductAttributeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.packshop.api.repositories.catalog.product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.packshop.api.entities.catalog.product.ProductAttribute;
-
-public interface ProductAttributeRepository extends JpaRepository<ProductAttribute, Long> {
-}

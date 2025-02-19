9f3f9ef4a8af74e0eaf786d69e4121dbd0988b1e
Nguyễn Trung Quân <nhotecfa@gmail.com>
2025-02-20 00:17:48 +0700

    Add category management page

---
+++ Client/src/main/java/com/packshop/client/modules/dashboard/catalog/controllers/category/CategoryManageController.java
@@ -0,0 +1,47 @@
+package com.packshop.client.modules.dashboard.catalog.controllers.category;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.packshop.client.common.utilities.ViewRenderer;
+import com.packshop.client.dto.catalog.CategoryDTO;
+import com.packshop.client.modules.dashboard.catalog.services.category.CategoryService;
+import com.packshop.client.modules.dashboard.catalog.services.product.ProductService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+@RequestMapping("/dashboard/catalog/categories")
+public class CategoryManageController {
+
+    private static final String CATALOG_PATH = "dashboard/pages/catalog";
+
+    private final ProductService productService;
+    private final CategoryService categoryService;
+
+    public CategoryManageController(ProductService productService, CategoryService categoryService) {
+        this.productService = productService;
+        this.categoryService = categoryService;
+    }
+
+    @GetMapping()
+    public String list(Model model) {
+        try {
+            log.info("Fetching all categories...");
+            List<CategoryDTO> categories = categoryService.getAllCategories();
+            model.addAttribute("categories", categories);
+        } catch (Exception e) {
+            log.error("Error fetching categories", e);
+            model.addAttribute("errorMessage", "Failed to fetch categories. Please try again.");
+        }
+
+        return ViewRenderer.renderView(model,
+                CATALOG_PATH + "/categories/list/index",
+                "Product List");
+    }
+}

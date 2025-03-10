497619a8701e349608f1a5b536180aa7bfaa32a1
Nguyễn Trung Quân <116139754+Quan2808@users.noreply.github.com>
2025-03-10 21:45:46 +0700

    update html

--- API/src/main/java/com/packshop/api/modules/shopping/address/controller/AddressController.java
+++ API/src/main/java/com/packshop/api/modules/shopping/address/controller/AddressController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.packshop.api.modules.identity.entities.User;
@@ -63,7 +62,6 @@ public class AddressController {
   }
 
   @DeleteMapping("/{addressId}")
-  @ResponseStatus(HttpStatus.NO_CONTENT)
   public ResponseEntity<Void> deleteAddress(
       @AuthenticationPrincipal User user,
       @PathVariable Long addressId) {
